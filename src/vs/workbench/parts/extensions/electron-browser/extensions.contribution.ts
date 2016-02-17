a349567e2746d61af2b93d8104872a99e7a2c568
Joao Moreno <jomo@microsoft.com>
2016-02-17 18:06:05 +0100

    extensions viewlet: first steps

--- src/vs/workbench/parts/extensions/electron-browser/extensions.contribution.ts
+++ src/vs/workbench/parts/extensions/electron-browser/extensions.contribution.ts
@@ -4,7 +4,7 @@
  *--------------------------------------------------------------------------------------------*/
 
 import nls = require('vs/nls');
-import platform = require('vs/platform/platform');
+import { Registry } from 'vs/platform/platform';
 import { registerSingleton } from 'vs/platform/instantiation/common/extensions';
 import statusbar = require('vs/workbench/browser/parts/statusbar/statusbar');
 import { ExtensionsStatusbarItem, ExtensionTipsStatusbarItem } from 'vs/workbench/parts/extensions/electron-browser/extensionsWidgets';
@@ -13,31 +13,36 @@ import { GalleryService } from 'vs/workbench/parts/extensions/node/vsoGallerySer
 import { IWorkbenchContributionsRegistry, Extensions as WorkbenchExtensions } from 'vs/workbench/common/contributions';
 import { ExtensionsWorkbenchExtension } from 'vs/workbench/parts/extensions/electron-browser/extensionsWorkbenchExtension';
 import ConfigurationRegistry = require('vs/platform/configuration/common/configurationRegistry');
+import { IViewletService } from 'vs/workbench/services/viewlet/common/viewletService';
+import { IWorkbenchEditorService } from 'vs/workbench/services/editor/common/editorService';
+import { ViewletRegistry, Extensions as ViewletExtensions, ViewletDescriptor, ToggleViewletAction } from 'vs/workbench/browser/viewlet';
+import { IWorkbenchActionRegistry, Extensions as ActionExtensions } from 'vs/workbench/common/actionRegistry';
+import { SyncActionDescriptor } from 'vs/platform/actions/common/actions';
 
 // Register Gallery Service
 registerSingleton(IGalleryService, GalleryService);
 
 // Register Extensions Workbench Extension
-(<IWorkbenchContributionsRegistry>platform.Registry.as(WorkbenchExtensions.Workbench)).registerWorkbenchContribution(
+(<IWorkbenchContributionsRegistry>Registry.as(WorkbenchExtensions.Workbench)).registerWorkbenchContribution(
 	ExtensionsWorkbenchExtension
 );
 
 // Register Statusbar item
-(<statusbar.IStatusbarRegistry>platform.Registry.as(statusbar.Extensions.Statusbar)).registerStatusbarItem(new statusbar.StatusbarItemDescriptor(
+(<statusbar.IStatusbarRegistry>Registry.as(statusbar.Extensions.Statusbar)).registerStatusbarItem(new statusbar.StatusbarItemDescriptor(
 	ExtensionsStatusbarItem,
 	statusbar.StatusbarAlignment.LEFT,
 	10 /* Low Priority */
 ));
 
 // Register Statusbar item
-(<statusbar.IStatusbarRegistry>platform.Registry.as(statusbar.Extensions.Statusbar)).registerStatusbarItem(new statusbar.StatusbarItemDescriptor(
+(<statusbar.IStatusbarRegistry>Registry.as(statusbar.Extensions.Statusbar)).registerStatusbarItem(new statusbar.StatusbarItemDescriptor(
 	ExtensionTipsStatusbarItem,
 	statusbar.StatusbarAlignment.LEFT,
 	9 /* Low Priority */
 ));
 
 
-(<ConfigurationRegistry.IConfigurationRegistry>platform.Registry.as(ConfigurationRegistry.Extensions.Configuration)).registerConfiguration({
+(<ConfigurationRegistry.IConfigurationRegistry>Registry.as(ConfigurationRegistry.Extensions.Configuration)).registerConfiguration({
 	id: 'extensions',
 	type: 'object',
 	properties: {
@@ -48,3 +53,30 @@ registerSingleton(IGalleryService, GalleryService);
 		}
 	}
 });
+
+export var VIEWLET_ID = 'workbench.view.extensions';
+
+class OpenExtensionsViewletAction extends ToggleViewletAction {
+	public static ID = VIEWLET_ID;
+	public static LABEL = nls.localize('toggleExtensionsViewlet', "Show Extensions");
+
+	constructor(id: string, label: string, @IViewletService viewletService: IViewletService, @IWorkbenchEditorService editorService: IWorkbenchEditorService) {
+		super(id, label, VIEWLET_ID, viewletService, editorService);
+	}
+}
+
+// Register Action to Open Viewlet
+(<IWorkbenchActionRegistry> Registry.as(ActionExtensions.WorkbenchActions)).registerWorkbenchAction(
+	new SyncActionDescriptor(OpenExtensionsViewletAction, OpenExtensionsViewletAction.ID, OpenExtensionsViewletAction.LABEL),
+	nls.localize('view', "View")
+);
+
+// Register Viewlet
+(<ViewletRegistry>Registry.as(ViewletExtensions.Viewlets)).registerViewlet(new ViewletDescriptor(
+	'vs/workbench/parts/extensions/electron-browser/extensionsViewlet',
+	'ExtensionsViewlet',
+	VIEWLET_ID,
+	nls.localize('extensions', "Extensions"),
+	'git',
+	200
+));
---
+++ src/vs/workbench/parts/extensions/electron-browser/extensionsViewlet.ts
@@ -0,0 +1,230 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
+import nls = require('vs/nls');
+import { TPromise } from 'vs/base/common/winjs.base';
+import { IDisposable, disposeAll } from 'vs/base/common/lifecycle';
+import { isNumber } from 'vs/base/common/types';
+import { Builder } from 'vs/base/browser/builder';
+import { emmet as $, append, addClass, removeClass } from 'vs/base/browser/dom';
+import { Viewlet } from 'vs/workbench/browser/viewlet';
+import { Dimension } from 'vs/base/browser/builder';
+import { VIEWLET_ID } from './extensions.contribution';
+import { ITelemetryService } from 'vs/platform/telemetry/common/telemetry';
+import { IInstantiationService } from 'vs/platform/instantiation/common/instantiation';
+import { IRenderer, IDelegate, IFocusChangeEvent, ISelectionChangeEvent } from 'vs/base/browser/ui/list/list';
+import { List } from 'vs/base/browser/ui/list/listWidget';
+import { IExtension, IExtensionsService } from '../common/extensions';
+import { HighlightedLabel, IHighlight } from 'vs/base/browser/ui/highlightedlabel/highlightedLabel';
+import { ActionBar } from 'vs/base/browser/ui/actionbar/actionbar';
+import { matchesContiguousSubString } from 'vs/base/common/filters';
+
+interface ITemplateData {
+	root: HTMLElement;
+	displayName: HighlightedLabel;
+	version: HTMLElement;
+	installCount: HTMLElement;
+	author: HTMLElement;
+	actionbar: ActionBar;
+	description: HighlightedLabel;
+	disposables: IDisposable[];
+}
+
+interface IHighlights {
+	name: IHighlight[];
+	displayName: IHighlight[];
+	description: IHighlight[];
+}
+
+enum ExtensionState {
+	Uninstalled,
+	Installed,
+	Outdated
+}
+
+interface IExtensionEntry {
+	extension: IExtension;
+	highlights: IHighlights;
+	state: ExtensionState;
+}
+
+function extensionEquals(one: IExtension, other: IExtension): boolean {
+	return one.publisher === other.publisher && one.name === other.name;
+}
+
+function getHighlights(input: string, extension: IExtension): IHighlights {
+	const name = matchesContiguousSubString(input, extension.name) || [];
+	const displayName = matchesContiguousSubString(input, extension.displayName) || [];
+	const description = matchesContiguousSubString(input, extension.description) || [];
+
+	if (!name.length && !displayName.length && !description.length) {
+		return null;
+	}
+
+	return { name, displayName, description };
+}
+
+function extensionEntryCompare(one: IExtensionEntry, other: IExtensionEntry): number {
+	const oneInstallCount = one.extension.galleryInformation ? one.extension.galleryInformation.installCount : 0;
+	const otherInstallCount = other.extension.galleryInformation ? other.extension.galleryInformation.installCount : 0;
+	const diff = otherInstallCount - oneInstallCount;
+
+	if (diff !== 0) {
+		return diff;
+	}
+
+	return one.extension.displayName.localeCompare(other.extension.displayName);
+}
+
+class Delegate implements IDelegate<IExtension> {
+	getHeight() { return 48; }
+	getTemplateId() { return 'extension'; }
+}
+
+class Renderer implements IRenderer<IExtensionEntry, ITemplateData> {
+
+	constructor(
+		@IInstantiationService private instantiationService: IInstantiationService,
+		@IExtensionsService private extensionsService: IExtensionsService
+	) {}
+
+	get templateId() { return 'extension'; }
+
+	renderTemplate(container: HTMLElement): ITemplateData {
+		// Important to preserve order here.
+		const root = append(container, $('.extension'));
+		const firstRow = append(root, $('.row'));
+		const secondRow = append(root, $('.row'));
+		const published = append(firstRow, $('.published'));
+		const displayName = new HighlightedLabel(append(firstRow, $('span.name')));
+		const installCount = append(firstRow, $('span.installCount'));
+		const version = append(published, $('span.version'));
+		const author = append(published, $('span.author'));
+
+		return {
+			root,
+			author,
+			displayName,
+			version,
+			installCount,
+			actionbar: new ActionBar(append(secondRow, $('.actions'))),
+			description: new HighlightedLabel(append(secondRow, $('span.description'))),
+			disposables: []
+		};
+	}
+
+	renderElement(entry: IExtensionEntry, index: number, data: ITemplateData): void {
+		const extension = entry.extension;
+		const publisher = extension.galleryInformation ? extension.galleryInformation.publisherDisplayName : extension.publisher;
+		const installCount = extension.galleryInformation ? extension.galleryInformation.installCount : null;
+		const actionOptions = { icon: true, label: false };
+
+		const updateActions = () => {
+			data.actionbar.clear();
+
+			if (entry.extension.galleryInformation) {
+				// data.actionbar.push(this.instantiationService.createInstance(OpenInGalleryAction, entry.state !== ExtensionState.Installed), { label: true, icon: false });
+			}
+
+			switch (entry.state) {
+				case ExtensionState.Uninstalled:
+					if (entry.extension.galleryInformation) {
+						// data.actionbar.push(this.instantiationService.createInstance(InstallAction, InstallLabel), actionOptions);
+					}
+					break;
+				case ExtensionState.Installed:
+					// data.actionbar.push(this.instantiationService.createInstance(UninstallAction), actionOptions);
+					break;
+				case ExtensionState.Outdated:
+					// data.actionbar.push(this.instantiationService.createInstance(UninstallAction), actionOptions);
+					// data.actionbar.push(this.instantiationService.createInstance(InstallAction, UpdateLabel), actionOptions);
+					break;
+			}
+		};
+
+		const onExtensionStateChange = (e: IExtension, state: ExtensionState) => {
+			if (extensionEquals(e, extension)) {
+				entry.state = state;
+				updateActions();
+			}
+		};
+
+		data.actionbar.context = extension;
+		updateActions();
+
+		data.disposables = disposeAll(data.disposables);
+		data.disposables.push(this.extensionsService.onDidInstallExtension(e => onExtensionStateChange(e, ExtensionState.Installed)));
+		data.disposables.push(this.extensionsService.onDidUninstallExtension(e => onExtensionStateChange(e, ExtensionState.Uninstalled)));
+
+		data.displayName.set(extension.displayName, entry.highlights.displayName);
+		data.displayName.element.title = extension.name;
+		data.version.textContent = extension.version;
+
+		if (isNumber(installCount)) {
+			data.installCount.textContent = String(installCount);
+			addClass(data.installCount, 'octicon');
+			addClass(data.installCount, 'octicon-cloud-download');
+
+			if (!installCount) {
+				data.installCount.title = nls.localize('installCountZero', "{0} wasn't downloaded yet.", extension.displayName);
+			} else if (installCount === 1) {
+				data.installCount.title = nls.localize('installCountOne', "{0} was downloaded once.", extension.displayName);
+			} else {
+				data.installCount.title = nls.localize('installCountMultiple', "{0} was downloaded {1} times.", extension.displayName, installCount);
+			}
+		} else {
+			data.installCount.textContent = '';
+			removeClass(data.installCount, 'octicon');
+			removeClass(data.installCount, 'octicon-cloud-download');
+		}
+
+		data.author.textContent = publisher;
+		data.description.set(extension.description, entry.highlights.description);
+		data.description.element.title = extension.description;
+	}
+
+	disposeTemplate(data: ITemplateData): void {
+		data.displayName.dispose();
+		data.description.dispose();
+		data.disposables = disposeAll(data.disposables);
+	}
+}
+
+export class ExtensionsViewlet extends Viewlet {
+
+	private domNode: HTMLElement;
+	private list: List<IExtensionEntry>;
+
+	constructor(
+		@IExtensionsService private extensionsService: IExtensionsService,
+		@IInstantiationService private instantiationService: IInstantiationService,
+		@ITelemetryService telemetryService: ITelemetryService
+	) {
+		super(VIEWLET_ID, telemetryService)
+	}
+
+	create(parent: Builder): TPromise<void> {
+		this.domNode = append(parent.getHTMLElement(), $('.extensions-viewlet'));
+		this.list = new List(this.domNode, new Delegate(), [this.instantiationService.createInstance(Renderer)]);
+
+		return this.extensionsService.getInstalled().then(extensions => {
+			const entries = extensions
+				.map(extension => ({ extension, highlights: getHighlights('', extension) }))
+				.filter(({ highlights }) => !!highlights)
+				.map(({ extension, highlights }) => ({
+					extension,
+					highlights,
+					state: ExtensionState.Installed
+				}))
+				.sort(extensionEntryCompare);
+
+			this.list.splice(0, this.list.length, ...entries);
+		});
+	}
+
+	layout(dimension: Dimension): void {
+		// console.log(dimension);
+	}
+}
